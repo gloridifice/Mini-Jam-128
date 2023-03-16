47a741111a717d489db9bf2efb03604045b66846
FlowerGolden <1327097302@qq.com>
2023-03-16 16:51:43 +0800

    add TrappedPerson to diedPerson after death

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Counter), typeof(Ending))]
 public class GameManager : MonoBehaviour
 {
     public TrappedPerson[] trappedPersons;
+    private Counter _counter;
 
     private Timer _timer;
 
     private void Start()
     {
         _timer = GetComponent<Timer>();
+        _counter = GetComponent<Counter>();
     }
 
     private void Update()
@@ -27,6 +30,7 @@ public class GameManager : MonoBehaviour
             if (person.health < _timer.Tick)
             {
                 person.status = PersonStatus.Died;
+                _counter.AddDiedPerson(person);
                 // Debug.Log("person " + person.name + " died");
             }
         }
